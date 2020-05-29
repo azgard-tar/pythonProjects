1a717a5f204f9737e49c9f925a37ce1c018fb4fe
azgard-tar <azgard-@outlook.com>
2020-05-29 14:45:29 +0300

    Extension for api. Adding user to db is added

--- learnApi/app/routes.py
+++ learnApi/app/routes.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import request
 import mysql.connector 
 import json
 
-def mysqlOn( query ):
+def mysqlGetSmth( query ):
     context = mysql.connector.connect(
         user = "pyuser",password="user",
         host="127.0.0.1",database="DB_for_learning"
@@ -14,23 +15,41 @@ def mysqlOn( query ):
     context.close()
     return ret
 
-@app.route('/')
-@app.route('/index')
+@app.route('/',methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
 def index():
-    data = mysqlOn( "SELECT Login,RealName,Email FROM Users" )
-    retArr = []
-    for (Login,RealName,Email) in data:
-        retArr.append({
-            "Login" : Login,
-            "RealName" : RealName,
-            "Email" : Email
-        })
-    
-    return json.dumps( retArr )   
+    if request.method == 'GET':
+        data = mysqlGetSmth( "SELECT Login,RealName,Email FROM Users" )
+        retArr = []
+        for (Login,RealName,Email) in data:
+            retArr.append({
+                "Login" : Login,
+                "RealName" : RealName,
+                "Email" : Email
+            })
+        
+        return json.dumps( retArr )  
+    else:
+        # Login, Registered, Email, RealName
+        context = mysql.connector.connect(
+            user = "pyuser",password="user",
+            host="127.0.0.1",database="DB_for_learning"
+        ) # connect to db
+        cursor = context.cursor()
+        cursor.execute( """ INSERT INTO Users 
+                            ( Login,RealName,Email,Registered )
+                            VALUES( %s, %s, %s, %s )
+        """, (request.form['Login'],request.form['RealName'],request.form['Email'],request.form['Registered']) )
+        id = cursor.lastrowid
+        context.commit()
+        
+        cursor.close()
+        context.close()
+        return "User added with id = " + str( id )
 
 @app.route('/<int:user_id>')
 def getById(user_id):
-    obj = mysqlOn("SELECT Login,RealName,Email FROM Users where Id = " + str( user_id ) )[0]
+    obj = mysqlGetSmth("SELECT Login,RealName,Email FROM Users where Id = " + str( user_id ) )[0]
     if obj != None:
         ret = {
             "Login" : obj[0],
